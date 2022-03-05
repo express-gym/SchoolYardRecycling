16de9f3c40bc47e1aaabc482253d850fc0680c4d
richardtmiles <richard@miles.systems>
2022-03-05 15:10:53 -0600

    updating proxy

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,6 +1,6 @@
 const {createProxyMiddleware} = require('http-proxy-middleware');
 
-const target = 'http://dev.carbonphp.com:8080/';
+const target = 'http://127.0.0.1:8080/';
 
 module.exports = function (app) {
     app.use('/rest/**', createProxyMiddleware({
@@ -9,40 +9,7 @@ module.exports = function (app) {
         secure: false,
         proxyTimeout: 4000,
         pathRewrite(path, req) {
-            return path.replace(/^\/rest/, 'rest')
-        },
-        logLevel: "debug"
-    }));
-    app.use(createProxyMiddleware([
-        '/view/**',
-        '!/' + process.env.REACT_APP_URI + '/**'
-    ],{
-        target: target,
-        changeOrigin: true,
-        secure: false,
-        proxyTimeout: 4000,
-        pathRewrite(path, req) {
-            return path.replace(/^\/view/, 'view')
-        },
-        logLevel: "debug"
-    }));
-    app.use('/view/releases/2.0.0/**', createProxyMiddleware({
-        target: target,
-        changeOrigin: true,
-        secure: false,
-        proxyTimeout: 4000,
-        pathRewrite(path, req) {
-            return path.replace(/^\/view\/releases\/2.0.0/, 'view/releases/2.0.0')
-        },
-        logLevel: "debug"
-    }));
-    app.use('/carbon/**', createProxyMiddleware({
-        target: target,
-        changeOrigin: true,
-        secure: false,
-        proxyTimeout: 4000,
-        pathRewrite(path, req) {
-            return path.replace(/^\/carbons/, 'carbons')
+            return path.replace(/^\/rest/, '/rest')
         },
         logLevel: "debug"
     }));
